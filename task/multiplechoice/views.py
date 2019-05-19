4b52366082fa1269be374e2ba944cd62b71b0af6
PadalaSrikanth <srikanthpadala36@gmail.com>
2019-05-20 01:37:44 +0530

    modified jquery functionality

--- task/multiplechoice/views.py
+++ task/multiplechoice/views.py
@@ -1,3 +1,4 @@
+import time
 from django.shortcuts import render
 from django.conf import settings
 from rest_framework.decorators import api_view
@@ -13,8 +14,11 @@ def add_track_question(request):
 @api_view(["POST"])
 def fibonacci_series(request):
     n = int(request.data["number"])
+    start_time = time.time()
     fibonacci_number = fibonacci(n)
-    data = {'fibonacci_number':fibonacci_number}
+    end_time = time.time()
+    excution_time = end_time-start_time
+    data = {'fibonacci_number':fibonacci_number, 'excution_time': excution_time}
     return Response(data, status=status.HTTP_200_OK)
 
 
@@ -30,19 +34,43 @@ def fibonacci_series(request):
 #         return temp_fib
 
 
-def fibonacci(n):
-    a = 0
-    b = 1
-    if n < 0:
-        print("Incorrect input")
-        return "Incorrect input"
-    elif n == 0:
-        return a
-    elif n == 1:
-        return b
+# def fibonacci(n):
+#     a = 0
+#     b = 1
+#     if n < 0:
+#         print("Incorrect input")
+#         return "Incorrect input"
+#     elif n == 0:
+#         return a
+#     elif n == 1:
+#         return b
+#     else:
+#         for i in range(2, n):
+#             c = a + b
+#             a = b
+#             b = c
+#         return b
+
+
+def fibonacci_helper(n, r):
+    """Return the nth Fibonacci number and store the ith Fibonacci number in
+    r[i] for 0 <= i <= n."""
+    if r[n] >= 0:
+        return r[n]
+
+    if (n == 0 or n == 1):
+        q = n
     else:
-        for i in range(2, n):
-            c = a + b
-            a = b
-            b = c
-        return b
+        q = fibonacci_helper(n - 1, r) + fibonacci_helper(n - 2, r)
+    r[n] = q
+
+    return q
+
+
+def fibonacci(n):
+    """Return the nth Fibonacci number."""
+    # r[i] will contain the ith Fibonacci number
+    r = [-1]*(n + 1)
+    return fibonacci_helper(n, r)
+
+
--- task/static/add-track-question-choices/add-track-question-choices.js
+++ task/static/add-track-question-choices/add-track-question-choices.js
@@ -1,15 +1,33 @@
-
+fibonacci = $('#question-div');
 function addTrackQuestion(){
-number = $('.number').val()
-console.log(number)
-$.ajax({
-    type: 'POST',
-    url: '/fibonacciseries/',
-    data: JSON.stringify ({"number":number}), // or JSON.stringify ({name: 'jonas'}),
-    contentType: "application/json",
-    dataType: 'json',
-    success: function(data) {
-        alert('fibonacci_number: ' + data["fibonacci_number"]);
-    }
-});
+    number = $('.number').val()
+
+
+    $.ajax({
+        type: 'POST',
+        url: '/fibonacciseries/',
+        data: JSON.stringify ({"number":number}), // or JSON.stringify ({name: 'jonas'}),
+        contentType: "application/json",
+        dataType: 'json',
+        success: function(data) {
+            var clone = "";
+            clone = fibonacci.clone();
+            fibonacci.empty()
+            clone.css('visibility', 'visible');
+            $('.fibanacci').append(clone);
+            $('.fibanacci_number').html(data["fibonacci_number"])
+            $('.fibanacci_time').html(data["excution_time"])
+
+        }
+    });
 }
+
+
+$('input[name="number"]').keyup(function(e)
+                                {
+  if (/\D/g.test(this.value))
+  {
+    // Filter non-digits from input value.
+    this.value = this.value.replace(/\D/g, '');
+  }
+});
